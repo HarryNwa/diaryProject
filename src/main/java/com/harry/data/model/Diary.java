a7f5d21e06eb7912ce59207e04f6514614dd5fd1
HarryNwa <harrynwa90@gmail.com>
2024-04-05 10:17:57 +0100

    Updated

--- src/main/java/com/harry/data/model/Diary.java
+++ src/main/java/com/harry/data/model/Diary.java
@@ -1,4 +1,15 @@
 package com.harry.data.model;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
 public class Diary {
+    @Id
+    private String id;
+    private String username;
+    private String password;
+    private boolean isLocked;
 }
--- src/main/java/com/harry/data/model/Entry.java
+++ src/main/java/com/harry/data/model/Entry.java
@@ -1,4 +1,14 @@
 package com.harry.data.model;
 
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@Document
 public class Entry {
+    @Id
+    private String id;
+    private String title;
+    private String body;
 }
